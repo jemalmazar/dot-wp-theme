4b170918a5825b1f9b569ab231ba8115dc291722
Jeremiah Almazar <jemalmazar@gmail.com>
2018-09-19 11:15:05 -0700

    Build event page

---
+++ wp-content/themes/doorsopentotechnology/events.php
@@ -0,0 +1,36 @@
+<?php
+/**
+* Template Name: Events
+*/
+
+get_header(); ?>
+
+    <div id="primary" class="content-area">
+            <main id="main" class="site-main">
+
+                <div class="hero-events">
+                    <img class="hero-logo-events" src="<?php echo get_template_directory_uri(); ?>/images/DOT_Tagline_Wh.png" alt="Doors Open to Technology Logo with tagline underneath">
+                
+                    <div class="event-details-container">
+                        <p class="event-date">October 26th, 2018</p>
+				        <p class="event-location">Vancouver, British Columbia</p>
+                    </div><!-- .event-details-container -->
+                </div><!-- .hero-events -->
+
+                <p class="dot-paragraph container">
+				    Doors Open to Technology events highlight the immense career opportunities available in the growing technology sector in British Columbia. High school students who attend our events leave with new information, inspiration and experiences that help to shape their future career path.
+                </p>
+                
+                <hr class="style1">
+
+                <div class="sched-agenda">
+                    <a id="sched-embed" href="//doorsopentotechnology2018.sched.com/" data-sched-sidebar="no">View the Doors Open to Technology schedule &amp; directory.</a>
+                    <script type="text/javascript" src="//doorsopentotechnology2018.sched.com/js/embed.js"></script>
+                </div>
+                
+                
+            </main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_footer();
