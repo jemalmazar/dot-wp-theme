80a838e1089898fd8d964a29efe78c41cfe991e8
Jeremiah Almazar <jemalmazar@gmail.com>
2018-09-24 23:09:15 -0700

    Update header logo and header bg colour

--- wp-content/themes/doorsopentotechnology/header.php
+++ wp-content/themes/doorsopentotechnology/header.php
@@ -26,23 +26,9 @@
 
 	<header id="masthead" class="site-header">
 		<div class="site-branding">
-			<img class="site-logo" src="<?php echo get_template_directory_uri(); ?>/images/DOT.png" alt="Doors Open to Technology Logo">
-			<!-- <?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$doorsopentotechnology_description = get_bloginfo( 'description', 'display' );
-			if ( $doorsopentotechnology_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $doorsopentotechnology_description; /* WPCS: xss ok. */ ?></p>
-			<?php endif; ?> -->
+			<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+				<img class="site-logo" src="<?php echo get_template_directory_uri(); ?>/images/DOT-logo-white.png" alt="Doors Open to Technology Logo">
+			</a>
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">
