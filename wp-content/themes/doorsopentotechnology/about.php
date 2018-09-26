cbdf76e4980d2220b4f476c01ed6b919a64a8e0c
Jeremiah Almazar <jemalmazar@gmail.com>
2018-09-25 23:11:33 -0700

    Adjust about page structure and styles

--- wp-content/themes/doorsopentotechnology/about.php
+++ wp-content/themes/doorsopentotechnology/about.php
@@ -9,19 +9,22 @@ get_header(); ?>
 		<main id="main" class="site-main">
 
 			<div class="hero-about">
-				<img class="hero-logo-about" src="<?php echo get_template_directory_uri(); ?>/images/DOT_Tagline_Wh.png" alt="Doors Open to Technology Logo with tagline underneath">
+
+                <div class="about-content">
+                    <img class="hero-logo-about" src="<?php echo get_template_directory_uri(); ?>/images/DOT_Tagline_Wh.png" alt="Doors Open to Technology Logo with tagline underneath">
             
-                <div class="about">
-                    <p class="about-p1">
-                    DOT gives high school students across the province access to a one-of-a-kind event experience.  At DOT events, students have the opportunity to go behind the doors of some of  B.C.’s coolest tech companies and explore careers in technology through real-world experiences.
-                    </p>
+                    <div class="about">
+                        <p class="about-p1">
+                        DOT gives high school students across the province access to a one-of-a-kind event experience.  At DOT events, students have the opportunity to go behind the doors of some of  B.C.’s coolest tech companies and explore careers in technology through real-world experiences.
+                        </p>
 
-                    <p class="about-p2">
-                        The intent is to connect high school students with the exciting career opportunities in BC’s growing tech sector. This unique event concept involves taking students ‘back-stage’ into the real-world of BC’s tech industry, creating memorable experiences that students can share with their classrooms and schools. 
-                    </p>
-                </div>
-
-            </div>
+                        <p class="about-p2">
+                            The intent is to connect high school students with the exciting career opportunities in BC’s growing tech sector. This unique event concept involves taking students ‘back-stage’ into the real-world of BC’s tech industry, creating memorable experiences that students can share with their classrooms and schools. 
+                        </p>
+                    </div><!-- .about -->
+                </div><!-- .about-content  -->
+                
+            </div><!-- .hero-about  -->
             
 
 		</main><!-- #main -->
