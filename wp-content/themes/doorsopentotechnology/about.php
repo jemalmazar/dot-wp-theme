242e0a359a1bf19116eccbb096f3b400ad3300ca
Jeremiah Almazar <jemalmazar@gmail.com>
2018-11-18 00:14:33 -0800

    Refactor about page

--- wp-content/themes/doorsopentotechnology/about.php
+++ wp-content/themes/doorsopentotechnology/about.php
@@ -12,15 +12,17 @@ get_header(); ?>
 
                 <div class="about-content">
                     <img class="hero-logo-about" src="<?php echo get_template_directory_uri(); ?>/images/DOT_Tagline_Wh.png" alt="Doors Open to Technology Logo with tagline underneath">
-            
-                    <div class="about">
-                        <p class="about-p1">
-                        DOT gives high school students across the province access to a one-of-a-kind event experience.  At DOT events, students have the opportunity to go behind the doors of some of  B.C.’s coolest tech companies and explore careers in technology through real-world experiences.
-                        </p>
 
-                        <p class="about-p2">
-                            The intent is to connect high school students with the exciting career opportunities in BC’s growing tech sector. This unique event concept involves taking students ‘back-stage’ into the real-world of BC’s tech industry, creating memorable experiences that students can share with their classrooms and schools. 
-                        </p>
+                    <div class="about">
+                        <?php if ( have_posts() ) : ?>
+
+                        <?php while ( have_posts() ) : the_post(); ?>
+
+                            <?php the_content(); ?>
+
+                        <?php endwhile; ?>
+
+                        <?php endif; ?>
                     </div><!-- .about -->
                 </div><!-- .about-content  -->
                 
